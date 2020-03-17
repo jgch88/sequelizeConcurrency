5d50a0b8abcf771562cb8b8c2dfda68908a10a7c
Justin Goh <justin.goh@thoughtworks.com>
2020-03-17 20:04:25 +0800

    added gatling simulation

---
+++ BasicSimulation.scala
@@ -0,0 +1,18 @@
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+class BasicSimulation extends Simulation {
+
+  val httpProtocol = http
+    .baseUrl("http://localhost:3000") // Here is the root for all relative URLs
+    .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
+
+  val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
+    .exec(
+      http("request_1")
+        .get("/add")
+    )
+
+  setUp(scn.inject(atOnceUsers(1000)).protocols(httpProtocol))
+}
